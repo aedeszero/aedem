0335abefd3bc0eda5a2ee12016a943ee310d665c
Carlos Henrique Rubia Barbosa <henriquerubiab@gmail.com>
2020-07-06 18:14:21 -0300

    Add privileges database model

---
+++ aedem/models/privileges.py
@@ -0,0 +1,30 @@
+import datetime
+
+from sqlalchemy import Column, String, DateTime, Boolean, text
+
+from aedem.models import Base
+
+class Privilege(Base):
+    __tablename__ = 'privileges'
+    
+    identifier      = Column(String,
+                        unique = True,
+                        nullable = False,
+                        primary_key = True)
+    assignable      = Column(Boolean,
+                        nullable = False,
+                        default = True)
+    last_updated    = Column(DateTime,
+                        nullable = False,
+                        server_default = text('NOW()'),
+                        onupdate = datetime.datetime.now)
+    created_at      = Column(DateTime,
+                        nullable = False,
+                        server_default = text('NOW()'))
+
+    def __init__(self, identifier, assignable = True):
+        self.identifier = identifier
+        self.assignable = assignable
+    
+    def __repr__(self):
+        return "<Privilege '{name}'>".format(name = self.identifier)
