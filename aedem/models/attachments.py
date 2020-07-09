daf5a7225ef543c89e7d26c586c2f843c961584e
kleitonv3 <kleitoncassio2017@outlook.com>
2020-07-08 22:19:51 -0300

    Indentation of models Report and Attachment adjusted

--- aedem/models/attachments.py
+++ aedem/models/attachments.py
@@ -10,16 +10,26 @@ from aedem.models import Base
 class Attachment(Base):
 	__tablename__ = 'attachments'
 
-	id = Column(Integer, primary_key = True)
-	# user_id = Column(UUID(as_uuid = True), ForeignKey('users.id'))
-	# user = relationship("User", back_populates="attachments")
-	report_id = Column(Integer, ForeignKey('reports.id'))
-	report = relationship("Report", back_populates="attachments")
-	attachment_addr = Column(String, nullable = False)
-	last_updated = Column(DateTime, onupdate = datetime.datetime.now)
-	created_at = Column(DateTime, nullable = False, server_default = text('NOW()'))
+	id 				= Column(Integer, 
+						primary_key = True)
+	# user_id 		= Column(UUID(as_uuid = True), 
+	# 					ForeignKey('users.id'))
+	# user 			= relationship("User", 
+	# 					back_populates="attachments")
+	report_id 		= Column(Integer, 
+						ForeignKey('reports.id'))
+	report 			= relationship("Report", 
+						back_populates="attachments")
+	attachment_addr = Column(String, 
+						nullable = False)
+	last_updated 	= Column(DateTime, 
+						server_default = text('NOW()'), 
+						onupdate = datetime.datetime.now)
+	created_at 		= Column(DateTime, 
+						nullable = False, 
+						server_default = text('NOW()'))
 
-	def __init__(self, attachment_addr):
+	def __init__(self, attachment_addr) -> None:
 		self.attachment_addr = attachment_addr
 
 	def __repr__(self) -> str:
--- aedem/models/reports.py
+++ aedem/models/reports.py
@@ -10,22 +10,39 @@ from aedem.models import Base
 class Report(Base):
 	__tablename__ = 'reports'
 
-	id = Column(Integer, primary_key = True)
-	# user_id = Column(UUID(as_uuid = True), ForeignKey('users.id'))
-	# user = relationship("User", back_populates="reports")
-	state_abbr = Column(String, nullable = False)
-	city_name = Column(String, nullable = False)
-	area = Column(String, nullable = True)
-	geolatitude = Column(String, nullable = True)
-	geolongitude = Column(String, nullable = True)
-	last_updated = Column(DateTime, onupdate = datetime.datetime.now)
-	created_at = Column(DateTime, nullable = False, server_default = text('NOW()'))
-	# id_ipaddr = Column(Integer, ForeignKey('ip_records.id'))
-	# ipdarr = relationship("IPRecord", back_populates="reports")
-	description = Column(String, nullable = True)
-	attachments = relationship('Attachment', back_populates='report')
+	id 				= Column(Integer, 
+						primary_key = True)
+	# user_id 		= Column(UUID(as_uuid = True),
+	# 					 ForeignKey('users.id'))
+	# user 			= relationship("User", 
+	# 					back_populates="reports")
+	state_abbr 		= Column(String, 
+						nullable = False)
+	city_name 		= Column(String, 
+						nullable = False)
+	area 			= Column(String, 
+						nullable = True)
+	geolatitude 	= Column(String, 
+						nullable = True)
+	geolongitude 	= Column(String, 
+						nullable = True)
+	last_updated 	= Column(DateTime, 
+						server_default = text('NOW()'), 
+						onupdate = datetime.datetime.now)
+	created_at 		= Column(DateTime, 
+						nullable = False, 
+						server_default = text('NOW()'))
+	# id_ipaddr 	= Column(Integer, 
+	# 					ForeignKey('ip_records.id'))
+	# ipdarr 		= relationship("IPRecord", 
+	# 					back_populates="reports")
+	description 	= Column(String, 
+						nullable = True)
+	attachments 	= relationship('Attachment', 
+						back_populates='report')
 
-	def __init__(self, state_abbr, city_name, area, geolatitude, geolongitude, description):
+	def __init__(self, state_abbr, city_name, area, geolatitude, geolongitude, 
+				description) -> None:
 		self.state_abbr = state_abbr
 		self.city_name = city_name
 		self.area = area
